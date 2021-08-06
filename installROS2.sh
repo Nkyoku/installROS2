b843593161df2bcb5eeff82dc32383770459e3ef
Nkyoku <nkyoku@gmail.com>
2021-08-06 09:06:47 +0900

    Update installROS2.sh

--- installROS2.sh
+++ installROS2.sh
@@ -53,6 +53,7 @@ sudo apt-get install -y --no-install-recommends \
 		python3-colcon-common-extensions \
 		python3-flake8 \
 		python3-pip \
+		python3-wheel \
 		python3-pytest-cov \
 		python3-rosdep \
 		python3-setuptools \
@@ -66,6 +67,7 @@ sudo rm -rf /var/lib/apt/lists/*
 # install some pip packages needed for testing
 python3 -m pip install -U \
 		argcomplete \
+		wheel \
 		flake8-blind-except \
 		flake8-builtins \
 		flake8-class-newline \
